b03459f1a0171eb75a058670114ec531fd6f6684
lenny <lennytmp@gmail.com>
2014-07-26 12:39:29 +0200

    get valuable neighbor

--- compiler/main.py
+++ compiler/main.py
@@ -72,8 +72,8 @@ class Program(CompilationUnit):
 
 
 #compilation_units = [Program('test'), Function('LOAD_DATA1')]
-compilation_units = [Program('main'), Function('STEP'), Function('MAP_AT'), Function('LIST_VAL_AT'),
-                    Function('GET_NEXT_DIRECTION'), Function('POINTS_EQ')]
+compilation_units = [Program('TEST'), Function('IS_VALUABLE'), Function('GET_VALUABLE_NEIGHBOR'), Function('MAP_AT'),
+                     Function('TEST_GET_VALUABLE_NEIGHBOR'), Function('LIST_VAL_AT'), Function('LOAD_DATA1')]
 
 if __name__ == '__main__':
     instruction_no = 0
